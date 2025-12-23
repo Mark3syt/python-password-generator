016c3b5db1b22e52d0e07e500c8deb4637779ab7
EMAA.xzy <161144843+Mark3syt@users.noreply.github.com>
2025-12-23 15:36:18 +0100

    Add files via upload

---
+++ python-password-generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def genera_password(lunghezza):
+    caratteri=string.ascii_letters+string.digits+"!@#$%^&*"
+    password=''
+    for i in range(lunghezza):
+        password+=random.choice(caratteri)
+    return password
+
+n=int(input("quante password? "))
+lunghezza=int(input("lunghezza: "))
+
+for i in range(n):
+    print(genera_password(lunghezza))
